e18b0f0a87d1f67a96784ea8458ec0939698569a
Kai Mashima <kaiomashima@gmail.com>
2017-05-10 16:12:10 -0400

    stringinator completed

--- stringinator.js
+++ stringinator.js
@@ -1,29 +1,31 @@
 const _ = require('./underbar');
 
 const first = function(str, n) {
-  // Your code goes here
+  return _.first(str, n);
 };
 
 const last = function(str, n) {
-  // Your code goes here
+  return _.last(str, n);
 };
 
 const removeChar = function(str, target) {
-  // hint: use _.reject
-  // Your code goes here
+  return (_.reject(str, (el) => el === target)).join('');
 };
 
 const hasChar = function(str, target) {
-  // hint: use _.some
-  // Your code goes here
+  return _.some(str, (el) => el === target);
 };
 
 const isOnlyDigits = function(str) {
-  // Your code goes here
+  if (isNaN(Number(str))) {
+    return false;
+  } else {
+    return _.every(Number(str), (el) => { return isNaN(el) === false });
+  }
 };
 
 const filterToOnlyDigits = function(str) {
-  // Your code goes here
+  return ((_.filter(str, (el) => isOnlyDigits(el))).join('')).replace(/ +/g, '');
 };
 
 const truncateString = function(val, maxLength) {
@@ -33,15 +35,23 @@ const truncateString = function(val, maxLength) {
 
 const truncateLongItems = function(obj, maxLength) {
   // hint: use truncateString above
-  // Your code goes here
+  return _.map(obj, (obj) => truncateString(obj, maxLength));
 };
 
 const countChars = function(str) {
-  // Your code goes here
+  return _.reduce(str, (obj, char) => {
+    if(obj[char] === undefined) {
+      obj[char] = 1;
+    } else {
+      obj[char]++;
+    }
+    return obj;
+  }, {});
 };
 
 const dedup = function(str) {
-  // Your code goes here
+  const obj = countChars(str);
+  return Object.keys(obj).join('');
 };
 
 module.exports = {
